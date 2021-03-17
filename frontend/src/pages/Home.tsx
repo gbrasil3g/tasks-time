36c17ed34a68b79a458752ed1af75b6e9f612466
edsonjscf <edsonjscf@gmail.com>
2021-03-17 17:30:22 -0300

    :sparkles: Updates on the home page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Box, Flex, Image, Text, Button } from '@chakra-ui/react'
-import { FcBusinessman, FcGoogle } from 'react-icons/fc'
+import { FcGoogle } from 'react-icons/fc'
+import { MdAccountCircle } from 'react-icons/md'
 
 export function Home () {
   return ( 
   <div>
     <main>
       <Box display="flex" alignItems="center" justifyContent="space-between" padding="90px">
-        <Image boxSize="550px" marginTop="80px"src="https://i.ibb.co/rH1MDWF/image.png" alt="Relogio Logo"/>
-        <Flex direction="column">
-            <Text fontSize="7xl" marginBottom="40px">Tasks Time</Text>
-            <Button leftIcon={<FcBusinessman />} size="lg">Create account</Button>
-            <Button leftIcon={<FcGoogle />} size="lg" marginTop="20px">Sign in with Google</Button>
+        <Image boxSize="520px" marginTop="80px" marginLeft="170px"src="https://i.ibb.co/rH1MDWF/image.png" alt="Relogio Logo"/>
+        <Flex direction="column" marginRight="260px">
+            <Text fontSize="7xl" width="185px">Tasks</Text>
+            <Text fontSize="7xl" marginBottom="40px" width="185px" marginLeft="30px" marginTop="-30px">Time</Text>
+            <Button leftIcon={<MdAccountCircle />} size="lg" width="280px" marginRight="10px" boxShadow="5px 7px 7px #c4c4c4">Create account</Button>
+            <Button leftIcon={<FcGoogle />} size="lg" marginTop="20px" width="280px" marginRight="10px" boxShadow="5px 7px 7px #c4c4c4">Sign in with Google</Button>
         </Flex>
       </Box>
     </main>
