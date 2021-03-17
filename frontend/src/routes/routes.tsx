2dfdc60d1f799cf2f4692db74199f29208e62f76
edsonjscf <edsonjscf@gmail.com>
2021-03-17 12:56:02 -0300

    :sparkles: New route created for the Login page.

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Switch, Redirect, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import { LoginAccount } from '../pages/LoginAccount'
 
 const Routes: React.FC = () => (
     <Switch>
       <Route path="/" exact component={Home}/>
+      <Route path="/login" exact component={LoginAccount}/>
       <Redirect to="/404" />
     </Switch>
 )
